45809c9865f93d2985e07407bf6393a05e059ab9
zengqz <954723922@qq.com>
2022-08-17 23:04:05 +0800

    增加配置参数解析器接口

---
+++ src/main/java/com/spring/core/env/ConfigurablePropertyResolver.java
@@ -0,0 +1,10 @@
+package com.spring.core.env;
+
+/**
+ * 配置参数解析器
+ */
+public interface ConfigurablePropertyResolver extends PropertyResolver{
+
+    void setRequiredProperties(String... requiredProperties);
+
+}
