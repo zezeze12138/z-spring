13e8b001e4afb8bf2d4fb330cb265a1d8b019de8
zengqz <954723922@qq.com>
2022-08-20 22:45:17 +0800

    增加refresh方法（步骤一）中prepareRefresh方法注释

--- src/main/java/com/spring/context/support/AbstractApplicationContext.java
+++ src/main/java/com/spring/context/support/AbstractApplicationContext.java
@@ -17,33 +17,40 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader i
      */
     @Override
     public synchronized void refresh() {
-        //准备上下文
+        //1.准备上下文
         prepareRefresh();
-        //告诉子类去刷新内部bean工厂
+        //2.告诉子类去刷新内部bean工厂
         ConfigurableListableBeanFactory beanFactory = obtainFreshBeanFactory();
-        //准备bean工厂在上下文中使用
+        //3.准备bean工厂在上下文中使用
         prepareBeanFactory(beanFactory);
-        //允许在上下文子类中对bean工厂进行后处理
+        //4.允许在上下文子类中对bean工厂进行后处理
         postProcessBeanFactory(beanFactory);
-        //调用在上下文中注册为bean的工厂处理器
+        //5.调用在上下文中注册为bean的工厂处理器
         invokeBeanFactoryPostProcessors(beanFactory);
-        //注册拦截bean创建的bean处理器
+        //6.注册拦截bean创建的bean处理器
         registerBeanPostProcessors(beanFactory);
-        //为上下文初始消息源
+        //7.为上下文初始消息源
         initMessageSource();
-        //为上下文初始事件多播
+        //8.为上下文初始事件多播
         initApplicationEventMulticaster();
-        //初始化特定上下文子类中的其他特定bean
+        //9.初始化特定上下文子类中的其他特定bean
         onRefresh();
-        //检测监听器bean并注册它们
-        //实例化所有剩余的非赖加载的单例
+        //10.检测监听器bean并注册它们
+        registerListeners();
+        //11.实例化所有剩余的非赖加载的单例
         finishBeanFactoryInitialization(beanFactory);
-        //发布相应事件
+        //12.发布相应事件
         finishRefresh();
     }
 
 
-
+    /**
+     * 准备上下文
+     * 创建一个Environment对象
+     * systemProperties:保存Java环境变量的键值
+     * systemEnvironment:保存了系统环境变量的键值
+     * 自定义properSource:保存了配置文件中的键值
+     */
     protected void prepareRefresh(){
 
     }
@@ -83,6 +90,11 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader i
     }
 
     protected void onRefresh() {
+
+
+    }
+
+    private void registerListeners() {
 
 
     }
