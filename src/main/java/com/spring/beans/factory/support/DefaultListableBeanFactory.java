16c3c32f1f28740631c26d60b9a8fcfa5f536913
zengqz <954723922@qq.com>
2022-08-29 02:52:19 +0800

    创建DefaultListableBeanFactory类

---
+++ src/main/java/com/spring/beans/factory/support/DefaultListableBeanFactory.java
@@ -0,0 +1,9 @@
+package com.spring.beans.factory.support;
+
+import com.spring.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * 默认列表的Bean工厂
+ */
+public class DefaultListableBeanFactory {
+}
