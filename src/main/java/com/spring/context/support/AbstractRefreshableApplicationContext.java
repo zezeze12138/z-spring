db04931f8a76b5ee01f8c9dbf35617d094d55b04
zengqz <954723922@qq.com>
2022-08-30 23:45:34 +0800

    创建AbstractRefreshableApplicationContext类

---
+++ src/main/java/com/spring/context/support/AbstractRefreshableApplicationContext.java
@@ -0,0 +1,41 @@
+package com.spring.context.support;
+
+import com.spring.beans.factory.config.ConfigurableListableBeanFactory;
+import com.spring.beans.factory.support.DefaultListableBeanFactory;
+
+public abstract class AbstractRefreshableApplicationContext extends AbstractApplicationContext{
+
+    private DefaultListableBeanFactory beanFactory;
+
+    public AbstractRefreshableApplicationContext(ClassLoader classLoader) {
+        super(classLoader);
+    }
+
+
+    @Override
+    protected ConfigurableListableBeanFactory getBeanFactory() {
+        return null;
+    }
+
+    @Override
+    protected void refreshBeanFactory() {
+        //查看是否已经创建过beanFactory，有创建就销毁
+        //创建BeanFactory
+        DefaultListableBeanFactory beanFactory = createBeanFactory();
+        //设置beanFactory属性参数
+        //设置自定义BeanFactory
+        customizeBeanFactory(beanFactory);
+        //加载beanDefinitions
+        loadBeanDefinitions(beanFactory);
+    }
+
+    private DefaultListableBeanFactory createBeanFactory() {
+        return new DefaultListableBeanFactory();
+    }
+
+    private void customizeBeanFactory(DefaultListableBeanFactory beanFactory) {
+
+    }
+
+    protected abstract void loadBeanDefinitions(DefaultListableBeanFactory beanFactory);
+}
