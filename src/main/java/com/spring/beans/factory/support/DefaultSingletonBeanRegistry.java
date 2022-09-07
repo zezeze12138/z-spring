d0811ca90a440f60c9736dab63234b9b55c0ec71
zengqz <954723922@qq.com>
2022-09-08 00:05:04 +0800

    增加DefaultSingletonBeanRegistry中getSingleton方法的处理逻辑

--- src/main/java/com/spring/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ src/main/java/com/spring/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -4,8 +4,10 @@ import com.spring.beans.factory.ObjectFactory;
 import com.spring.beans.factory.config.SingletonBeanRegistry;
 import com.spring.core.SimpleAliasRegistry;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements SingletonBeanRegistry {
@@ -25,6 +27,11 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
      */
     private final Map<String, Object> earlySingletonObjects = new HashMap<>(16);
 
+    /**
+     * 正在创建的bean集合
+     */
+    private final Set<String> singletonCurrentlyInCreation = Collections.newSetFromMap(new ConcurrentHashMap<>(16));
+
     @Override
     public void registerSingleton(String beanName, Object singletonObject) {
 
@@ -42,6 +49,38 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
      * @return
      */
     private Object getSingleton(String beanName, boolean allowEarlyReference) {
-        return null;
+        //先在已经构建的缓存中获取bean
+        Object singletonObject = this.singletonObjects.get(beanName);
+        //是否bean未构建且正在构建中
+        if(singletonObject == null && isSingletonCurrentlyInCreation(beanName)){
+            synchronized (this.singletonObjects){
+                //是否在早期bean缓存中存在bean
+                singletonObject = this.earlySingletonObjects.get(beanName);
+                //如果早期bean缓存中不存在bean且允许进行早期的Bean属性引用
+                if(singletonObject == null && allowEarlyReference){
+                    //获取构建bean的工厂
+                    ObjectFactory<?> singletonFactory = this.singletonFactories.get(beanName);
+                    //如果构建bean的工厂不为空，则通过工厂创建bean
+                    if(singletonFactory != null){
+                        //通过工厂创建bean
+                        singletonObject = singletonFactory.getObject();
+                        //将bean放入早期缓存中
+                        this.earlySingletonObjects.put(beanName, singletonObject);
+                        //将单例bean的创建工厂在集合移除
+                        this.singletonFactories.remove(beanName);
+                    }
+                }
+            }
+        }
+        return singletonObject;
+    }
+
+    /**
+     * 是否Bean在存在中
+     * @param beanName
+     * @return
+     */
+    public boolean isSingletonCurrentlyInCreation(String beanName){
+        return this.singletonCurrentlyInCreation.contains(beanName);
     }
 }
