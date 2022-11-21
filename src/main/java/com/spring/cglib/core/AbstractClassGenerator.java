049afa5ff24d7fa722fa15e26a8f8b74bce21139
zengqz <954723922@qq.com>
2022-11-21 22:40:23 +0800

    增加ClassLoaderData内部类

--- src/main/java/com/spring/cglib/core/AbstractClassGenerator.java
+++ src/main/java/com/spring/cglib/core/AbstractClassGenerator.java
@@ -1,6 +1,12 @@
 package com.spring.cglib.core;
 
 
+import java.lang.ref.WeakReference;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
  * @Author: zengqz
  * @Description: TODO
@@ -14,6 +20,8 @@ public abstract class AbstractClassGenerator implements ClassGenerator {
     private GeneratorStrategy strategy =  DefaultGeneratorStrategy.INSTANCE;
 
     private NamingPolicy namingPolicy = DefaultNamingPolicy.INSTANCE;
+
+    private Object key;
 
     public void setContextClass(Class contextClass){
         this.contextClass = contextClass;
@@ -31,5 +39,21 @@ public abstract class AbstractClassGenerator implements ClassGenerator {
             namingPolicy = DefaultNamingPolicy.INSTANCE;
         this.namingPolicy = namingPolicy;
     }
+    protected static class ClassLoaderData {
+        private final Set<String> reservedClassNames = new HashSet<String>();
 
+        private final WeakReference<ClassLoader> classLoader;
+
+        private final Predicate uniqueNamePredicate = name -> reservedClassNames.contains(name);
+
+        private static final Function<AbstractClassGenerator, Object> GET_KEY = new Function<AbstractClassGenerator, Object>() {
+            public Object apply(AbstractClassGenerator gen) {
+                return gen.key;
+            }
+        };
+
+        public ClassLoaderData(ClassLoader classLoader, WeakReference<ClassLoader> classLoader1) {
+            this.classLoader = classLoader1;
+        }
+    }
 }
